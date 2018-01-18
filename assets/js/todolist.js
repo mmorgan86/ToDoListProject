7364c8a5b567fc47934aabbc766aa6923cfb8ac4
mmorgan86 <michael1986morgan@gmail.com>
2018-01-18 11:01:48 -0500

    Todo List project

---
+++ assets/js/todolist.js
@@ -0,0 +1,29 @@
+// Check Off Specific Todos By Clicking
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed");
+});
+
+// Click On X To Delete ToDo
+$("div").on("click", "span", function(event){
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  
+  //Stop the click event from happing to the whole "li"
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(){
+  if(event.which === 13){
+    //grabbing new todo text from input
+    var todoText = $(this).val();
+    $(this).val("");
+    //creat a new li and add to ul
+    //use apend() method
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+  }
+});
+
+$(".fa-pencil-square-o").click(function(){
+  $("input[type='text']").fadeToggle();
+});
